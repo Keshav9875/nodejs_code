7273ab970994544fda79e7b5d83eb1391e8e4fa5
keshav Garg <garg68860@gmail.com>
2024-10-30 01:37:40 +0530

    stream in nodejs

---
+++ stream.js
@@ -0,0 +1,35 @@
+const http=require("http");
+const fs=require("fs");
+
+const server=http.createServer((req,res)=>{
+//    fs.readFile('./data.txt','utf-8',(err,data)=>{
+//      if(err){
+//         res.end(err);
+//      }
+//      res.end(data);
+//    })
+
+//streaming example
+
+    const rstream=fs.createReadStream("./data.txt");
+    
+    rstream.on("data",(readdata)=>{
+        res.write(readdata);
+    })
+
+    rstream.on("end" ,()=>{
+        res.end();
+    })
+
+    rstream.on("error",()=>{
+        res.end("file not found");
+    })
+
+    res.on("finish",()=>{
+        console.log("Response has been fully sent to the client...");
+    })
+})
+let port=3300;
+server.listen(port,"localhost",()=>{
+    console.log("Listening port no "+`${port}`);
+})
