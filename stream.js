293d4e3ef9038fb88bdca3e6cc3344164779e8f3
keshav Garg <garg68860@gmail.com>
2024-10-30 23:22:02 +0530

    stream file

--- stream.js
+++ stream.js
@@ -11,24 +11,32 @@ const server=http.createServer((req,res)=>{
 
 //streaming example
 
-    const rstream=fs.createReadStream("./data.txt");
+    // const rstream=fs.createReadStream("./data.txt");
     
-    rstream.on("data",(readdata)=>{
-        res.write(readdata);
-    })
+    // rstream.on("data",(readdata)=>{
+    //     res.write(readdata);
+    // })
 
-    rstream.on("end" ,()=>{
-        res.end();
-    })
+    // rstream.on("end" ,()=>{
+    //     res.end();
+    // })
 
-    rstream.on("error",()=>{
-        res.end("file not found");
-    })
+    // rstream.on("error",()=>{
+    //     res.end("file not found");
+    // })
 
-    res.on("finish",()=>{
-        console.log("Response has been fully sent to the client...");
-    })
+    // res.on("finish",()=>{
+    //     console.log("Response has been fully sent to the client...");
+    // })
+
+
+    // by using stream pipes it is easy and fast also in compare to previious method.
+    const rstream=fs.createReadStream("./data.txt");
+    rstream.pipe(res);
+    
+
 })
+
 let port=3300;
 server.listen(port,"localhost",()=>{
     console.log("Listening port no "+`${port}`);
